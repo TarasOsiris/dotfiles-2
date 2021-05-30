23e3d48c86d7a9a292f109681d1b26b99ffe1192
Taras Leskiv <leskiv.taras@gmail.com>
2021-05-30 21:41:34 +0300

    more

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ sudo pacman -S zsh vim --noconfirm
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
+chsh -s $(which zsh)
