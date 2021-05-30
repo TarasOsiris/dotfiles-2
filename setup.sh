b94f67a358d4adb5d299201b196ffb853aa1e02b
Taras Leskiv <leskiv.taras@gmail.com>
2021-05-30 22:16:49 +0300

    symlink

--- setup.sh
+++ setup.sh
@@ -12,3 +12,15 @@ sudo pacman -S zsh vim stow --noconfirm
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s $(which zsh)
+
+# symlink
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo "Script folder: $SCRIPTPATH"
+cd $SCRIPTPATH
+
+rm -f $HOME/.vimrc
+ln -s $SCRIPTPATH/.vimrc $HOME/.vimrc
+
+rm -f $HOME/.zshrc
+ln -s $SCRIPTPATH/.zshrc $HOME/.ideavimrc
