5ad9579d615e475d25051b84d76d439355d8fcbf
Taras Leskiv <leskiv.taras@gmail.com>
2021-06-01 11:32:21 +0300

    df

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,5 @@
+alias df='cd ~/dotfiles'
+
 # GIT
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip--"'
 alias gwipp='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip--"; git push'
