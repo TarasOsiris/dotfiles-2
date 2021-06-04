fc285eec6df2d8c6a7593392d4a9bd3c41c28003
Taras Leskiv <leskiv.taras@gmail.com>
2021-06-04 19:10:45 +0300

    --wip--

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ git clone git@github.com:MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/
 # vim
 # Plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall
 
 # symlink
 
